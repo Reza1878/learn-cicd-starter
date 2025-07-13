50863e55f95e56b4509ee5f08b567969282d0485
Reza Rizqi Ramdhani <rezarizqir@gmail.com>
2025-07-13 09:41:31 +0700

    update: add auth test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetApiKey(t *testing.T) {
+	type result struct {
+		apiKey string
+		err    error
+	}
+
+	tests := map[string]struct {
+		input http.Header
+		want  result
+	}{
+		"no token provided": {
+			input: map[string][]string{},
+			want:  result{apiKey: "", err: errors.New("no authorization header included")},
+		},
+		"malformed authorization header": {
+			input: map[string][]string{"Authorization": {"Bearer invalidapikey"}},
+			want:  result{apiKey: "", err: errors.New("malformed authorization header")},
+		},
+		"valid authorization header": {
+			input: map[string][]string{"Authorization": {"ApiKey FakeApiKey"}},
+			want:  result{apiKey: "ApiKey", err: nil},
+		},
+	}
+
+	for name, tc := range tests {
+		res, err := GetAPIKey(tc.input)
+
+		if res != tc.want.apiKey && tc.want.err != err {
+			t.Fatalf("%s: expected %v, got %v", name, tc.want, result{apiKey: res, err: err})
+		}
+	}
+}
